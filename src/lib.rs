e164436e2e04ce4f4a20f437446ba402d708f374
creatormind <63318104+creatormind-dev@users.noreply.github.com>
2024-11-01 23:27:29 -0600

    Added init function for the controller

--- src/lib.rs
+++ src/lib.rs
@@ -41,48 +41,46 @@ pub fn init(pins: bsp::Pins) {
 
 	// These are the switches of the buttons.
 
+	let sw_start_pin = pins.gpio14.into_pull_up_input().into_dyn_pin();
 	let sw_bt_a_pin = pins.gpio2.into_pull_up_input().into_dyn_pin();
 	let sw_bt_b_pin = pins.gpio4.into_pull_up_input().into_dyn_pin();
 	let sw_bt_c_pin = pins.gpio6.into_pull_up_input().into_dyn_pin();
 	let sw_bt_d_pin = pins.gpio8.into_pull_up_input().into_dyn_pin();
 	let sw_fx_l_pin = pins.gpio10.into_pull_up_input().into_dyn_pin();
 	let sw_fx_r_pin = pins.gpio12.into_pull_up_input().into_dyn_pin();
-	let sw_start_pin = pins.gpio14.into_pull_up_input().into_dyn_pin();
 
 	// These are the lamp holders/LEDs of the buttons.
 
+	let led_start_pin = pins.gpio15.into_push_pull_output().into_dyn_pin();
 	let led_bt_a_pin = pins.gpio3.into_push_pull_output().into_dyn_pin();
 	let led_bt_b_pin = pins.gpio5.into_push_pull_output().into_dyn_pin();
 	let led_bt_c_pin = pins.gpio7.into_push_pull_output().into_dyn_pin();
 	let led_bt_d_pin = pins.gpio9.into_push_pull_output().into_dyn_pin();
 	let led_fx_l_pin = pins.gpio11.into_push_pull_output().into_dyn_pin();
 	let led_fx_r_pin = pins.gpio13.into_push_pull_output().into_dyn_pin();
-	let led_start_pin = pins.gpio15.into_push_pull_output().into_dyn_pin();
 
 	/* ~~ GPIO/PINOUT CONFIGURATION END ~~ */
 
+	let button_start = Button::new(sw_start_pin, led_start_pin);
 	let button_bt_a = Button::new(sw_bt_a_pin, led_bt_a_pin);
 	let button_bt_b = Button::new(sw_bt_b_pin, led_bt_b_pin);
 	let button_bt_c = Button::new(sw_bt_c_pin, led_bt_c_pin);
 	let button_bt_d = Button::new(sw_bt_d_pin, led_bt_d_pin);
 	let button_fx_l = Button::new(sw_fx_l_pin, led_fx_l_pin);
 	let button_fx_r = Button::new(sw_fx_r_pin, led_fx_r_pin);
-	let button_start = Button::new(sw_start_pin, led_start_pin);
 
 	// Turns the integrated LED on once the controller is plugged-in.
 	pico_led_pin.set_high().unwrap();
 
-	unsafe {
-		*CONTROLLER = Some(Controller {
-			start: button_start,
-			bt_a: button_bt_a,
-			bt_b: button_bt_b,
-			bt_c: button_bt_c,
-			bt_d: button_bt_d,
-			fx_l: button_fx_l,
-			fx_r: button_fx_r
-		});
-	}
+	Controller::init(
+		button_start,
+		button_bt_a,
+		button_bt_b,
+		button_bt_c,
+		button_bt_d,
+		button_fx_l,
+		button_fx_r,
+	)
 }
 
 
@@ -100,11 +98,35 @@ pub struct Controller {
 }
 
 impl Controller {
+	fn init(
+		start: Button,
+		bt_a: Button,
+		bt_b: Button,
+		bt_c: Button,
+		bt_d: Button,
+		fx_l: Button,
+		fx_r: Button
+	) {
+		unsafe {
+			*CONTROLLER = Some(Self {
+				start,
+				bt_a,
+				bt_b,
+				bt_c,
+				bt_d,
+				fx_l,
+				fx_r,
+			})
+		}
+	}
+
 	/// Handles button presses using debouncing and updates the LEDs in the buttons accordingly.
 	pub fn update(&mut self, syst: &SYST) {
 		// Gets the amount of microseconds elapsed since the device booted.
 		let current_time = syst.cvr.read();
 		let buttons = self.buttons();
+
+		buttons.reverse();
 
 		for button in buttons {
 			let is_pressed = button.switch.is_pressed();
@@ -144,13 +166,13 @@ impl Controller {
 
 	fn buttons(&mut self) -> [&mut Button; BT_SIZE] {
 		[
+			&mut self.start,
 			&mut self.bt_a,
 			&mut self.bt_b,
 			&mut self.bt_c,
 			&mut self.bt_d,
 			&mut self.fx_l,
 			&mut self.fx_r,
-			&mut self.start,
 		]
 	}
 }
